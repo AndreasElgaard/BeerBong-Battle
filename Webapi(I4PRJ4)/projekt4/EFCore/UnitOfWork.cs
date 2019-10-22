f302dc1eeb74371a2fee1412e40737f85e1b2dce
Mads Stengaard Jørgensen <au592631@uni.au.dk>
2019-10-22 21:36:03 +0200

    Unitofwork pattern added

---
+++ Webapi(I4PRJ4)/projekt4/EFCore/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using projekt4.Repositories;
+using projekt4.EFCore;
+
+namespace projekt4.EFCore
+{
+    public class UnitOfWork
+    {
+        private EFCoreBrugerRepository _EFCoreBrugerRepository;
+        private EFCoreRegisterRepository _EFCoreRegisterRepository;
+        private BBMcontex
+    }
+}
