85f9882f9329a21aa0d5cac0e6779132c376f6c6
Mads Stengaard Jørgensen <au592631@uni.au.dk>
2019-10-23 22:54:20 +0200

    intial commit

--- Webapi(I4PRJ4)/projekt4/Repositories/RegisterRepository.cs
+++ Webapi(I4PRJ4)/projekt4/Repositories/RegisterRepository.cs
@@ -8,49 +8,49 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace projekt4.Repositories
-{
-    public class RegisterRepository : IRegisterRepository
-    {
-        private readonly BBMContext _Context;
-        public RegisterRepository(BBMContext context)
-        {
-            _Context = context ?? throw new ArgumentNullException(nameof(context));
-        }
+//namespace projekt4.Repositories
+//{
+//    public class RegisterRepository : IRegisterRepository
+//    {
+//        private readonly BBMContext _Context;
+//        public RegisterRepository(BBMContext context)
+//        {
+//            _Context = context ?? throw new ArgumentNullException(nameof(context));
+//        }
 
-        public Task<ActionResult<Register>> CreateAsync(Register register)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<ActionResult<Register>> CreateAsync(Register register)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<ActionResult<Register>> DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<ActionResult<Register>> DeleteAsync(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<ActionResult<Register>> GetOneRegisterAsync(int id)
-        {
-            var register = _Context.Register.Find(id);
+//        public Task<ActionResult<Register>> GetOneRegisterAsync(int id)
+//        {
+//            var register = _Context.Register.Find(id);
 
-            if (register == null)
-                return null;
+//            if (register == null)
+//                return null;
 
-            return register;
-        }
+//            return register;
+//        }
 
-        public Task<IEnumerable<Register>> GetRegisterAsync()
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<IEnumerable<Register>> GetRegisterAsync()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<bool> IsRegisterExitsAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+//        public Task<bool> IsRegisterExitsAsync(int id)
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        public Task<IActionResult> UpdateAsync(Register register)
-        {
-            throw new NotImplementedException();
-        }
-    }
-}
+//        public Task<IActionResult> UpdateAsync(Register register)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
