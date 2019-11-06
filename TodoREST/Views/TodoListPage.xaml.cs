5187442e7c7b6f2fdcdcb940825b6775f42cef06
Mathias Tørnes Pedersen <au591121@uni.au.dk>
2019-11-06 11:46:10 +0100

    Ændringer

--- TodoREST/Views/TodoListPage.xaml.cs
+++ TodoREST/Views/TodoListPage.xaml.cs
@@ -19,24 +19,24 @@ namespace TodoREST
 			listView.ItemsSource = await App.TodoManager.GetTasksAsync ();
 		}
 
-		async void OnAddItemClicked (object sender, EventArgs e)
-		{
-            await Navigation.PushAsync(new TodoItemPage(true)
-            {
-                BindingContext = new BrugerTest
-                {
+		//async void OnAddItemClicked (object sender, EventArgs e)
+		//{
+  //          await Navigation.PushAsync(new TodoItemPage(true)
+  //          {
+  //              BindingContext = new BrugerTest
+  //              {
                     
 
-                }
-            });
-		}
+  //              }
+  //          });
+		//}
 
-		async void OnItemSelected (object sender, SelectedItemChangedEventArgs e)
-		{
-            await Navigation.PushAsync(new TodoItemPage
-            {
-                BindingContext = e.SelectedItem as BrugerTest
-            });
-		}
+		//async void OnItemSelected (object sender, SelectedItemChangedEventArgs e)
+		//{
+  //          await Navigation.PushAsync(new TodoItemPage
+  //          {
+  //              BindingContext = e.SelectedItem as BrugerTest
+  //          });
+		//}
 	}
 }
